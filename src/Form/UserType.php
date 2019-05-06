db965659ebcfcff1b3161c7f97d1176058ac952a
Snow Crash <hello@snowcrash.fun>
2019-05-06 21:40:16 +0200

    Move button outside of form class (best practice)

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
@@ -16,9 +16,13 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, ['attr'=>array('placeholder'=>'Username')])
-            ->add('password', PasswordType::class, ['attr'=>array('placeholder'=>'Password')])
-            ->add('save', SubmitType::class, ['label'=>'Sign Up!'])
-        ;
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Passwords must match',
+                'first_options' => ['attr'=>array('placeholder'=>'Password')],
+                'second_options' => ['attr'=>array('placeholder'=>'Repeat password')]
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
