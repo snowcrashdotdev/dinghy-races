2fee4522815dff68bd908e4fd226f5c124aa135a
Snow Crash <hello@snowcrash.fun>
2019-05-06 13:07:52 +0200

    Add admin controller with basic dashboard, page layout

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\TournamentType;
 
 /**
  * @Route("/admin")
@@ -15,8 +16,7 @@ class AdminController extends AbstractController
      */
     public function index()
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+
+        return $this->render('admin/index.html.twig');
     }
 }
