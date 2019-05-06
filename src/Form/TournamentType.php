ed44f1b531f3b7c3d6043b211f308d772bcf1575
Snow Crash <hello@snowcrash.fun>
2019-05-06 13:09:06 +0200

    Update TournamentType form class

--- src/Form/TournamentType.php
+++ src/Form/TournamentType.php
@@ -6,14 +6,19 @@ use App\Entity\Tournament;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class TournamentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class, ['attr'=>array('placeholder'=>'Title')])
+            ->add('start_date', DateType::class)
+            ->add('end_date', DateType::class)
+            ->add('description', TextareaType::class, ['attr'=>array('placeholder'=>'Description')])
         ;
     }
 
