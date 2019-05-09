51af119f45c435561e4d50977fe650f002a1aae0
Snow Crash <hello@snowcrash.fun>
2019-05-09 21:48:39 +0200

    Add default user role

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -48,6 +48,7 @@ class User implements UserInterface
     public function __construct()
     {
         $this->scores = new ArrayCollection();
+        $this->roles = ['ROLE_USER']
     }
 
     public function getId(): ?int
