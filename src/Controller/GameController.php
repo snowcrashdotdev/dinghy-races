481df65003e050e4a2ef4ea4a5dfefd43670a53c
Snow Crash <hello@snowcrash.fun>
2019-05-12 08:48:19 +0200

    Add Game CRUD

---
+++ src/Controller/GameController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use App\Form\GameType;
+use App\Repository\GameRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/games")
+ * @isGranted("ROLE_ADMIN")
+ */
+class GameController extends AbstractController
+{
+    /**
+     * @Route("/", name="game_index", methods={"GET"})
+     */
+    public function index(GameRepository $gameRepository): Response
+    {
+        return $this->render('game/index.html.twig', [
+            'games' => $gameRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="game_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $game = new Game();
+        $form = $this->createForm(GameType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($game);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('game_index');
+        }
+
+        return $this->render('game/new.html.twig', [
+            'game' => $game,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="game_show", methods={"GET"})
+     */
+    public function show(Game $game): Response
+    {
+        return $this->render('game/show.html.twig', [
+            'game' => $game,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="game_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Game $game): Response
+    {
+        $form = $this->createForm(GameType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('game_index', [
+                'id' => $game->getId(),
+            ]);
+        }
+
+        return $this->render('game/edit.html.twig', [
+            'game' => $game,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="game_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Game $game): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$game->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($game);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('game_index');
+    }
+}
