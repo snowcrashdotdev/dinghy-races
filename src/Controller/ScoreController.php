0fc873d946ebc5e5c75ba81c12fb497b287e5916
Snow Crash <hello@snowcrash.fun>
2019-05-12 08:34:59 +0200

    We can add scores to games now
    TODO: Only one score per user per game

---
+++ src/Controller/ScoreController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+use App\Entity\Score;
+use App\Form\ScoreType;
+
+
+/**
+ * @Route("/score")
+ */
+class ScoreController extends AbstractController
+{
+    /**
+     * @Route("/new/{game}/{tournament}", name="score_new", methods={"GET","POST"})
+     */
+    public function new(Request $request, $game, $tournament): Response
+    {
+        $game = $this->getDoctrine()
+            ->getRepository('App\Entity\Game')
+            ->find($game);
+
+        $tournament = $this->getDoctrine()
+            ->getRepository('App\Entity\Tournament')
+            ->find($tournament);
+
+        $score = new Score($game, $tournament, $this->getUser());
+        $form = $this->createForm(ScoreType::class, $score);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($score);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('tournament_scores',
+                [
+                    'id'=>$tournament->getId(),
+                    'game'=>$game->getId()
+                ]
+            );
+        }
+
+        return $this->render('score/new.html.twig', [
+            'score' => $score,
+            'form' => $form->createView()
+        ]);
+    }
+}
