43909ecd37a08e43d45927a3488093a2c1c4674e
Snow Crash <hello@snowcrash.fun>
2019-05-15 00:55:37 +0200

    Update tournament form and JS
    Render nested collections, and use AJAX search to find usernames
    TODO: Include results in UI

--- public/js/tournament-admin.js
+++ public/js/tournament-admin.js
@@ -1,13 +1,90 @@
+var teamCollection
+var teamSection
+var addTeamButton = document.createElement('button')
+addTeamButton.type = 'button'
+addTeamButton.innerText = 'Add team'
+addTeamButton.classList.add('add-team-member-button')
+var userSearchUrl;
+
 window.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('.add-team-to-tournament').addEventListener('click', function(e) {
-        var teamList = document.querySelector(e.target.dataset.listSelector)
-        var newTeam = teamList.dataset.prototype
-        var i = teamList.childElementCount
-        newTeam = newTeam.replace(/__name__/g, i)
-        i++
-        teamList.dataset.widgetCounter = i
-        var newListItem = document.createElement(teamList.dataset.tag)
-        newListItem.innerHTML = newTeam
-        teamList.append(newListItem)
+    teamCollection = document.querySelector('#teams-collection')
+    teamSection = document.querySelector('#teams')
+    teamSection.appendChild(addTeamButton)
+    teamCollection.dataset.index = teamCollection.childElementCount
+    userSearchUrl = teamSection.dataset.ajaxUrl
+
+    addTeamButton.addEventListener('click', function(e) {
+        addTeamForm(teamCollection)
     })
+
+    addHandlerToButtons()
+    addSearchToElements()
 })
+
+function addTeamForm(collection) {
+    var prototype = collection.dataset.prototype
+    var index = collection.dataset.index
+    var form = prototype.replace(/__team__/g, index)
+    collection.dataset.index++
+    var newTeam = document.createElement('li')
+    newTeam.innerHTML= form
+    collection.appendChild(newTeam)
+
+    var addMemberButton = document.createElement('button')
+    addMemberButton.type = 'button'
+    addMemberButton.innerText = 'Add member'
+    addMemberButton.classList.add('add-team-member')
+    addMemberButton.addEventListener('click', handleAddMember)
+
+
+    newTeam.appendChild(addMemberButton).dataset.collection = newTeam.firstElementChild.id + '_members'
+}
+
+function handleAddMember() {
+    var membersCollection = document.getElementById(this.dataset.collection)
+    membersCollection.dataset.index = membersCollection.childElementCount
+    addMemberForm(membersCollection)
+}
+
+function addMemberForm(collection) {
+    var prototype = collection.dataset.prototype
+    var index = collection.dataset.index
+    var form = prototype.replace(/__member__/g, index)
+    var newMember = document.createRange().createContextualFragment(form)
+    collection.appendChild(newMember)
+    addSearchToElements()
+}
+
+function searchForUser() {
+    var q = this.value
+    if (q.length > 3) {
+        var url = userSearchUrl + '/' + q
+        window.fetch(url, {method: 'POST'})
+            .then(function(res){
+                return res.json()
+            })
+            .then(function(json){
+                console.info(json)
+            })
+    } else {
+        return false;
+    }
+}
+
+function addSearchToElements() {
+    var searchElements = document.getElementsByClassName('user-search')
+    if (searchElements) {
+        for (var i = 0; i < searchElements.length; i++) {
+            searchElements[i].addEventListener('keyup', searchForUser, false)
+        }
+    }
+}
+
+function addHandlerToButtons() {
+    var addTeamMemberButtons = document.getElementsByClassName('add-team-member-button')
+    if (addTeamMemberButtons) {
+        for (var i = 0; i < addTeamMemberButtons.length; i++) {
+            addTeamMemberButtons[i].addEventListener('click', handleAddMember)
+        }
+    }
+}
