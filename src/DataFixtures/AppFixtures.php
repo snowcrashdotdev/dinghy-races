b7732d563d56d6c4e5254f741b2186a09653aefd
Snow Crash <hello@snowcrash.fun>
2019-05-15 00:56:45 +0200

    Scale back the App fixtures
    We only need one tournament to test with

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,11 +5,11 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\DataFixtures\UserFixtures;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 
 use App\Entity\Tournament;
 
-class AppFixtures extends Fixture implements DependentFixtureInterface
+class AppFixtures extends Fixture implements FixtureGroupInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -20,29 +20,11 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
         $upcoming_tournament->setEndDate(new \DateTime('2019-06-30'));
 
         $manager->persist($upcoming_tournament);
-
-        $past_tournament = new Tournament();
-        $past_tournament->setTitle('2019 NFL Playoffs');
-        $past_tournament->setDescription('Tom Brady ya bitch.');
-        $past_tournament->setEndDate(new \DateTime('-1 MONTH'));
-        $past_tournament->setStartDate(new \DateTime('-2 MONTH'));
-
-        $manager->persist($past_tournament);
-
-        $in_progress_tournament = new Tournament();
-        $in_progress_tournament->setTitle('Kappa Open 2019');
-        $in_progress_tournament->setDescription('<p>Put your database to the test.</p>');
-        $in_progress_tournament->setStartDate(new \DateTime('-15 DAY'));
-        $in_progress_tournament->setEndDate(new \DateTime('+15 DAY'));
-
-        $manager->persist($in_progress_tournament);
         $manager->flush();
     }
 
-    public function getDependencies()
+    public static function getGroups(): array
     {
-        return array(
-            UserFixtures::class
-        );
+        return ['dev'];
     }
 }
