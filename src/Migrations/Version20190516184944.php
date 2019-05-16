805107da13cbac0c5ce4e6a3c0400896a921773a
Snow Crash <hello@snowcrash.fun>
2019-05-16 21:15:27 +0200

    Rename foreign key in score table (again)

---
+++ src/Migrations/Version20190516184944.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190516184944 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE score DROP FOREIGN KEY FK_32993751BF7AB635');
+        $this->addSql('DROP INDEX IDX_32993751BF7AB635 ON score');
+        $this->addSql('ALTER TABLE score CHANGE tourney_team_id team_id INT NOT NULL');
+        $this->addSql('ALTER TABLE score ADD CONSTRAINT FK_32993751296CD8AE FOREIGN KEY (team_id) REFERENCES team (id)');
+        $this->addSql('CREATE INDEX IDX_32993751296CD8AE ON score (team_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE score DROP FOREIGN KEY FK_32993751296CD8AE');
+        $this->addSql('DROP INDEX IDX_32993751296CD8AE ON score');
+        $this->addSql('ALTER TABLE score CHANGE team_id tourney_team_id INT NOT NULL');
+        $this->addSql('ALTER TABLE score ADD CONSTRAINT FK_32993751BF7AB635 FOREIGN KEY (tourney_team_id) REFERENCES team (id)');
+        $this->addSql('CREATE INDEX IDX_32993751BF7AB635 ON score (tourney_team_id)');
+    }
+}
