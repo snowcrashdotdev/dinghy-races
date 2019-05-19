56487725070fcb29275c1a78748b80ee1d823588
Snow Crash <hello@snowcrash.fun>
2019-05-19 22:16:25 +0200

    Add constraints to new user form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
@@ -21,8 +24,24 @@ class UserType extends AbstractType
                     'placeholder'=>'Username'
                 ]
             ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Email address'
+                ]
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 4096,
+                    ]),
+                ],
                 'invalid_message' => 'Passwords must match',
                 'first_options' => [
                     'attr'=> ['placeholder'=>'Password'],
