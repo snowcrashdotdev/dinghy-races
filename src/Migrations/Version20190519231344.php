a82de716737d02d308b6dfdb790d7d937ee11156
Snow Crash <hello@snowcrash.fun>
2019-05-20 01:35:56 +0200

    Use correct class name for later migration
    Apparently this, and not the filename, is how versions are determined

--- src/Migrations/Version20190519231344.php
+++ src/Migrations/Version20190519231344.php
@@ -9,7 +9,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190514144008 extends AbstractMigration
+final class Version20190519231344 extends AbstractMigration
 {
     public function getDescription() : string
     {
