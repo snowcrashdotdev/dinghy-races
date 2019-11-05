f6a241cddfca5d598e2d7e774e8993e5110462dc
Snow Crash <hello@snowcrash.fun>
2019-11-05 11:45:13 -0500

    Sort User scores by date updated by default

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -54,6 +54,7 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Score", mappedBy="user", orphanRemoval=true)
+     * @ORM\OrderBy({"date_updated" = "DESC"})
      */
     private $scores;
 
