fe642ec82761c862f87091260d05507aab2ce7eb
Snow Crash <hello@snowcrash.fun>
2019-11-06 10:44:39 -0500

    Add TwitchChecker service
    Polls Twitch API for live streams and caches the resulting array
    
    Returns false if array is empty

---
+++ src/Service/TwitchChecker.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\Cache\Adapter\PdoAdapter;
+use Doctrine\DBAL\Driver\Connection;
+use Symfony\Contracts\Cache\ItemInterface;
+use App\Entity\Tournament;
+
+class TwitchChecker
+{
+    private $cache;
+    private $clientId;
+
+    public function __construct(Connection $connection, String $clientId)
+    {
+        $this->cache = new PdoAdapter(
+            $connection,
+            'twitch-',
+            60
+        );
+        $this->clientId = $clientId;
+    }
+
+    public function getLiveTwitchStreams(Tournament $tournament)
+    {
+        $liveStreams = $this->cache->get('live_streams', function(ItemInterface $item) use ($tournament) {
+            $item->expiresAfter(60);
+            $users = $tournament->getUsers()->filter(function($user) {
+                return $user->getProfile()->hasTwitch();
+            })->toArray();
+    
+            $twitchUsers = array_map(
+                function($user) {
+                    $url = parse_url($user->getProfile()->getSocial());
+    
+                    return trim($url['path'], '/');
+                }, $users
+            );
+    
+            $query = '?user_login=' . join('&user_login=', $twitchUsers);
+    
+            $remote_api = 'https://api.twitch.tv/helix/streams';
+    
+            $curl_url = $remote_api . $query;
+            $header = 'Client-ID: ' . $this->clientId;
+            $curl_opt = array(
+                CURLOPT_URL => $curl_url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => array($header)
+            );
+            $curl = curl_init();
+            curl_setopt_array($curl, $curl_opt);
+            $json = json_decode(curl_exec($curl));
+            curl_close($curl);
+            
+    
+            $live = array_filter($json->data, function($stream){
+                return $stream->type === 'live';
+            });
+
+            return $live;
+        });
+
+        if (empty($liveStreams)) {
+            return false;
+        } else {
+            return $liveStreams;
+        }
+    }
+}
