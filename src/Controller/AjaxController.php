d88f34d4f3d1c09341535c4d50f84ece00a03686
Snow Crash <hello@snowcrash.fun>
2020-03-30 04:13:29 -0400

    Add ajax controller with (currently) one route, serving a substring search for games
    The idea is that all the methods in this controller
    only respond to XH requests

---
+++ src/Controller/AjaxController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/ajax")
+ */
+class AjaxController extends AbstractController
+{
+    /**
+     * @Route("/search/game/{query}")
+     */
+    public function search_game(Request $request, ?string $query, SerializerInterface $serializer)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $games = $this->getDoctrine()
+                ->getRepository('App\Entity\Game')
+                ->findByLike($query);
+
+            $data = $serializer->serialize(
+                $games,
+                'json',
+                ['groups' => 'public']
+            );
+
+            return $this->json($data);
+        }
+    }
+}
