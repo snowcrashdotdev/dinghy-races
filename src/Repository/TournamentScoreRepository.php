7391db9b3944f80d70e7fa5083d5fd0a86371046
Snow Crash <hello@snowcrash.fun>
2020-07-31 16:53:34 -0400

    Add method to run raw SQL query to update aggregate statistics for a given tournament

--- src/Repository/TournamentScoreRepository.php
+++ src/Repository/TournamentScoreRepository.php
@@ -195,6 +195,39 @@ class TournamentScoreRepository extends ServiceEntityRepository
         return $q->getQuery()->getResult();
     }
 
+    public function updateAggregateStatsFor(Tournament $tournament)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+            UPDATE tournament_user AS u,
+            (
+                SELECT
+                    s.user_id as user_id,
+                    AVG(s.rank) AS avg_rank,
+                    SUM(s.ranked_points) AS ranked_points,
+                    SUM(s.team_points) AS team_points,
+                    SUM(CASE WHEN s.auto_assigned = 1 THEN 0 ELSE 1 END) AS submissions
+                    FROM score s
+                    WHERE s.type = \'tournament\'
+                    AND s.tournament_id = :tournament
+                    GROUP BY s.user_id
+            ) AS stats
+            SET
+            u.avg_rank = stats.avg_rank,
+            u.ranked_points = stats.ranked_points,
+            u.team_points = stats.team_points,
+            u.avg_rank = stats.avg_rank,
+            u.completion = stats.submissions
+            WHERE u.id = stats.user_id
+            AND u.tournament_id = :tournament
+        ';
+
+        $statement = $conn->prepare($sql);
+
+        $statement->execute(['tournament' => $tournament->getId()]);
+    }
+
     // /**
     //  * @return Score[] Returns an array of Score objects
     //  */
