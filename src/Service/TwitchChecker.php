8e6aa5ff06d9656dc76fb8cf9ade115d4cbdb589
Snow Crash <hello@snowcrash.fun>
2020-08-01 13:13:38 -0400

    Add OAuth to requests for data from Twitch

--- src/Service/TwitchChecker.php
+++ src/Service/TwitchChecker.php
@@ -12,11 +12,12 @@ class TwitchChecker
     public const CACHE_EXPIRES = 180;
     private $cache;
     private $clientId;
+    private $clientSecret;
     private $streamTag;
     private $manager;
     private $cache_handle = 'live_streams';
 
-    public function __construct(Connection $connection, String $clientId, String $streamTag, EntityManagerInterface $manager)
+    public function __construct(Connection $connection, String $clientId, String $clientSecret, String $streamTag, EntityManagerInterface $manager)
     {
         $this->cache = new PdoAdapter(
             $connection,
@@ -24,15 +25,20 @@ class TwitchChecker
             60
         );
         $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
         $this->streamTag = $streamTag;
         $this->manager = $manager;
     }
 
     public function getLiveStreams(Tournament $tournament)
     {
+        $token = $this->getOAuthToken();
+
+        if ($token === null) { return []; }
+
         $this->setCacheHandle( 'live_streams_' . $tournament->getId() );
 
-        $liveStreams = $this->cache->get($this->getCacheHandle(), function(ItemInterface $item) use ($tournament) { 
+        $liveStreams = $this->cache->get($this->getCacheHandle(), function(ItemInterface $item) use ($tournament, $token) { 
             $item->expiresAfter($this::CACHE_EXPIRES);
 
             $twitch_users = $this->manager
@@ -50,24 +56,27 @@ class TwitchChecker
     
             $remote_api = 'https://api.twitch.tv/helix/streams';
     
-            $curl_url = $remote_api . $query;
-            $header = 'Client-ID: ' . $this->clientId;
-            $curl_opt = array(
-                CURLOPT_URL => $curl_url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_HTTPHEADER => array($header)
-            );
-            $curl = curl_init();
-            curl_setopt_array($curl, $curl_opt);
-            $json = json_decode(curl_exec($curl));
-            curl_close($curl);
+            $url = $remote_api . $query;
 
-            if (property_exists($json, 'data')) {
+            $header = [
+                'Client-ID: ' . $this->clientId,
+                'Authorization: Bearer ' . $token
+            ];
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_HTTPGET, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = json_decode(curl_exec($ch), true);
+            curl_close($ch);
+
+            if (isset($response['data'])) {
                 $tag = strtolower($this->streamTag);
-                $live = array_filter($json->data, function($stream) use ($tag) {
+                $live = array_filter($response['data'], function($stream) use ($tag) {
                     return (
-                        $stream->type === 'live' &&
-                        ! (strpos(strtolower($stream->title), $tag) === false)
+                        $stream['type'] === 'live' &&
+                        ! (strpos(strtolower($stream['title']), $tag) === false)
                     );
                 });
     
@@ -84,12 +93,36 @@ class TwitchChecker
         }
     }
 
-    public function getCacheHandle()
+    private function getOAuthToken()
+    {
+        $token = $this->cache->get('token', function(ItemInterface $item) {
+            $item->expiresAfter(4722000);
+
+            $url = "https://id.twitch.tv/oauth2/token?client_id={$this->clientId}&client_secret={$this->clientSecret}&grant_type=client_credentials";
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = json_decode(curl_exec($ch), true);
+            curl_close($ch);
+
+            if (isset($response['access_token'])) {
+                return $response['access_token'];
+            } else {
+                return null;
+            }
+        });
+
+        return $token;
+    }
+
+    private function getCacheHandle()
     {
         return $this->cache_handle;
     }
 
-    public function setCacheHandle(String $handle)
+    private function setCacheHandle(String $handle)
     {
         $this->cache_handle = $handle;
 
