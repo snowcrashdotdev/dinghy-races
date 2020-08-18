421bab3939b1b187f7da2a0b955cd9d3c89860dc
Snow Crash <hello@snowcrash.fun>
2020-08-18 09:48:58 -0400

    Sort games so each only appears once

--- assets/js/components/SuggestedGames.vue
+++ assets/js/components/SuggestedGames.vue
@@ -3,7 +3,7 @@
         <h2>Suggested Games</h2>
         <p>Find games where you have a good chance of ranking up.</p>
         <label for="difficulty">Difficulty</label>
-        <input class="range" type="range" name="difficulty" v-model="difficulty" min="0.01" max="0.25" step="0.005">
+        <input class="range" type="range" name="difficulty" v-model="difficulty" min="0" max="100" step="1">
 
         <div class="suggestions">
             <ul>
@@ -15,12 +15,7 @@
 
 <style scoped>
 .suggestions {
-    overflow-x: hidden;
-    overflow-y: scroll;
-}
-
-ul {
-    max-height: 100%;
+    overflow: hidden;
 }
 
 .range::-moz-range-track {
@@ -41,18 +36,31 @@ export default {
     name: 'suggested-games',
     data() {
         return {
-            difficulty: 0.01
+            difficulty: 0
         }
     },
     props: ['user', 'scores', 'stddev'],
     computed: {
-        suggestions: function() {
-            let deviations = this.scores.filter(s => this.user.scores.filter(s2 => s2.game.id === s.game.id && s.points > s2.points).length >= 1 && this.user.username !== s.user.username).map(s => ({
-                title: s.game.title,
-                dev: Math.abs(s.points - this.user.scores.find(s2 => s2.game.id === s.game.id).points)
-            }))
+        userStdDevs: function() {
+            let scores = this.scores.filter(s => this.user.username !== s.user.username).sort((s1, s2) => s1.points - s2.points)
+            
+            return this.user.scores.map(s => {
+                let sNext = scores.find(n => n.game.id === s.game.id && n.points > s.points)
+                let stddev = this.stddev.find(d => s.game.title === d.title).stddev
 
-            return this.stddev.filter(d => deviations.some(d2 => d2.title === d.title && (d2.dev / d.stddev) <= this.difficulty)).sort((d,d2) => d.stddev - d2.stddev).slice(-4)
+                return {
+                    title: s.game.title,
+                    stddev: Math.abs(sNext.points - s.points) / stddev
+                }
+            })
+            .sort((d1,d2) => d1.stddev - d2.stddev)
+        },
+        suggestions: function() {
+            let count = this.userStdDevs.length
+            let slice = 4
+            let offset = 100 - this.difficulty
+            let i = Math.floor((count * 100 - count * offset) / 100);
+            return this.userStdDevs.slice(i,i+slice)
         }
     }
 }
